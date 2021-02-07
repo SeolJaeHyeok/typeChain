c605912b8870dd3ae02b63878df1b9f799d08c2c
seoljaehyeok <milkboy2564@naver.com>
2021-02-07 20:54:28 +0900

    Interface 대신 class를 사용

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import { StringDecoder } from "string_decoder";
-
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name;
+  public age;
+  public gender;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Seol",
-  age: 22,
-  gender: "male",
-};
+const seol = new Human("jaehyeok", 28, "male");
 
 const sayHello = (person: Human): string => {
   return `Hello, ${person.name}, you are ${person.age} years, you are a ${person.gender}`;
 };
 
-console.log(sayHello(person));
+console.log(sayHello(seol));
 
 export {};
